62698ed3effce73462b7e850eecb26d6241cda1c
Waldemar <41773009+integral96@users.noreply.github.com>
2019-03-26 10:14:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "lib.h"
 #include <iostream>
 
-#include <spdlog/spdlog.h>
-#include "spdlog/sinks/stdout_sinks.h"
+//#include <spdlog/spdlog.h>
+//#include "spdlog/sinks/stdout_sinks.h"
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    //auto logger = spdlog::stdout_logger_mt("console");
+    //logger->info("version {} was started", version());
+    std::cout << "build " << TRAVIS_BUILD_NUMBER <<std::endl;
     std::cout << "HELLO WORLD!" << '\n';
     return 0;
 }
